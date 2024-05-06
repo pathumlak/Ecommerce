59e534d6852a6fcff6087aef919491558e83abf4
pathumlak <123624162+pathumlak@users.noreply.github.com>
2024-05-06 23:39:31 +0530

    updated

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -8,10 +8,14 @@ import {
 } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navigation.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useLoginMutation } from "../../redux/api/userApiSlice";
+import { logout } from "../../redux/features/auth/authSlice";
 
 const Navigation = () => {
+  const { userInfo } = useSelector((state) => state.auth);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
 
@@ -23,6 +27,20 @@ const Navigation = () => {
   };
   const closeSidebar = () => {
     setShowSidebar(false);
+  };
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLoginMutation();
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -63,6 +81,19 @@ const Navigation = () => {
           <span className="hidden nav-item-name mt-[3rem]">FAVOURITES</span>
         </Link>
       </div>
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center text-gray-800 focus:outline-none"
+        >
+          {userInfo ? (
+            <span className="text-white">{userInfo.username}</span>
+          ) : (
+            <></>
+          )}
+        </button>
+      </div>
+
       <ul>
         <li>
           <Link
